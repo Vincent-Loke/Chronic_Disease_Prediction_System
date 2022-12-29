0f02fb6d63d4d868a9bf9b27ba50fbb1bd94aa78
Vincent-Loke <115884322+Vincent-Loke@users.noreply.github.com>
2022-12-29 21:43:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+#--------------------------------------------------------------------------------------------------------------------------------------------------#
+####################################################################################################################################################
+# Execution of all pages for streamlit                                                                                                             #
+# Developer Name: Loke Weng Khay                                                                                                                   #
+####################################################################################################################################################
+#--------------------------------------------------------------------------------------------------------------------------------------------------#
 import streamlit as st
 
-
+# import all pages
 from predict_stroke_page import show_predict_stroke_page
 from predict_lung_cancer_page import show_predict_lung_cancer_page
 from predict_heart_disease_page import show_predict_heart_disease_page
@@ -8,6 +14,7 @@ from Chronic_Diseases import chronic_disease_page,lung_cancer_analysis_page,stro
 
 page = st.sidebar.selectbox("Explore Different Chronic Disease Prediction System",("Chronic Diseases","Analysis of Lung Cancer","Analysis of Stroke","Analysis of Heart Disease","Predict Lung Cancer","Predict Stroke","Predict Heart Disease"))
 
+# selection page
 if page == "Chronic Diseases":
     chronic_disease_page()
 elif page == "Analysis of Lung Cancer":
